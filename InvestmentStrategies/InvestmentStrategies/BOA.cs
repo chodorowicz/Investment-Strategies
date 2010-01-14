fb60960e3bc167a8968b9b1c7e3bbe0a8c2fc524
kubaszumiato <kuba.szumiato@gmail.com>
2010-01-14 23:12:50 +0100

    Changed common functions implementation file name to CommonFunctions.cs and added some new stuff

--- InvestmentStrategies/InvestmentStrategies/BOA.cs
+++ InvestmentStrategies/InvestmentStrategies/BOA.cs
@@ -7,6 +7,28 @@ namespace InvestmentStrategies
 {
     class BOA
     {
+        private int populationSize;
+        private int chromosomeLength;
+
+        public BOA(int popS, int chrL)
+        {
+            this.populationSize = popS;
+            this.chromosomeLength = chrL;
+        }
+
+        public int[] BayesianOptimisationAlgorithm()
+        {
+            int[][] population = new int[this.populationSize][];
+
+            for(int i = 0; i < populationSize; i++)
+                for(int j = 0; j < chromosomeLength; j++)
+                {
+                    population[i] = (CommonFunctions.RandomIndividual(CommonFunctions.InitialProbabilityVector(chromosomeLength));
+                }
+
+
+            return population[0];
+        }
 
     }
 }
