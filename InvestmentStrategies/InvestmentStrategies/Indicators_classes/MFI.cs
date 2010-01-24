0bf75df2fd588c4e828dbb161252f6e61b5efdb9
Jakub Chodorowicz <chodorowicz@gmail.com>
2010-01-25 00:34:40 +0100

    kolejny wskaznik: Money Flow Index - mamy juz ich wiecej niz team Szymona i Marcina mial :)

---
+++ InvestmentStrategies/InvestmentStrategies/Indicators_classes/MFI.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InvestmentStrategies
+{
+    public class MFI : AbstractIndicator, IIndicator
+    {
+        public Indicators indicators;
+        public int period;
+        public double[] typicalPrice;
+        public double[] rawMoneyFlow;
+        public double[] positiveMoneyFlow;
+        public double[] negativeMoneyFlow;
+
+
+        public MFI(Indicators indicators, int period)
+        {
+            this.period = period;
+            this.indicators = indicators;
+            this.data = new double[this.indicators.stockData.Count];
+            this.typicalPrice = new double[this.indicators.stockData.Count];
+            this.rawMoneyFlow = new double[this.indicators.stockData.Count];
+            this.positiveMoneyFlow = new double[this.indicators.stockData.Count];
+            this.negativeMoneyFlow = new double[this.indicators.stockData.Count];
+
+            this.calculate();
+        }
+
+        public double decide(int day)
+        {
+            if (data[day] > 80) return -1.0;      // overbought, sell!
+            else if (data[day] < 20) return 1.0;  // oversold, buy!    
+            else return 0.0;                      // no decision      
+        }
+
+        private void calculate()
+        {
+            double positive;
+            double negative;
+            double moneyRatio;
+
+            for (int i = 0; i < indicators.stockData.Count; i++)
+            {
+                typicalPrice[i] = (indicators.stockData[i]["high"]
+                                    + indicators.stockData[i]["low"]
+                                    + indicators.stockData[i]["close"]) / 3;
+                rawMoneyFlow[i] = typicalPrice[i] * indicators.stockData[i]["volume"];
+            }
+
+            for (int i = period; i < indicators.stockData.Count(); i++)
+            {
+                positiveMoneyFlow[i] = 0.0;
+                negativeMoneyFlow[i] = 0.0;
+                for (int j = i - period + 1; j <= period; j++)
+                {
+                    if (typicalPrice[j] > typicalPrice[j - 1])
+                        positiveMoneyFlow[i] += rawMoneyFlow[j];
+                    else if (typicalPrice[j] < typicalPrice[j - 1])
+                        negativeMoneyFlow[i] += rawMoneyFlow[j];
+                }
+                if (negativeMoneyFlow[i] != 0.0)
+                {
+                    moneyRatio = positiveMoneyFlow[i] / negativeMoneyFlow[i];
+                    data[i] = 100 - (100 / (1 + moneyRatio));
+                }
+                
+            }
+
+
+        }
+    }
+}
---
+++ InvestmentStrategies/InvestmentStrategiesTests/Indicators_Tests/MFI_Test.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InvestmentStrategies;
+
+namespace InvestmentStrategiesTests.Indicators_Tests
+{
+    /// <summary>
+    /// Summary description for MFI_Test
+    /// </summary>
+    [TestClass]
+    public class MFI_Test
+    {
+        private static readonly string pathToTestData = "../../../InvestmentStrategiesTests/data-test/data-test.txt";
+        private static readonly double delta = 0.001;
+        private InvestmentStrategies.Indicators indicators;
+
+        public MFI_Test()
+        {
+            indicators = new InvestmentStrategies.Indicators();
+            this.indicators.readData(pathToTestData);
+        }
+
+
+        [TestMethod]
+        public void MFI_Test_General()
+        {
+            MFI mfi = new MFI(indicators, 20);
+            Assert.AreEqual(10.0, mfi.rawMoneyFlow[10], delta);
+            Assert.AreEqual(2 * 10.0, mfi.positiveMoneyFlow[20], delta);
+            Assert.AreEqual(0.0, mfi.negativeMoneyFlow[20], delta);
+        }
+    }
+}
